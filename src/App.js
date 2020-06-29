5cd121534d6abed607e9359c0916d9b03dfa11b6
ceress <markmesina69@gmail.com>
2020-06-29 06:17:24 -0700

    complete component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <ReactPlayer url ='https://www.youtube.com/watch?v=7sDY4m8KNLc'/>
+      <ReactPlayer
+        width='480px'
+        height='240px'
+        controls
+        url ='https://www.youtube.com/watch?v=7sDY4m8KNLc'
+        onReady={() => console.log('onReady callback')}
+        onStart={() => console.log('onStart callback')}
+        onPause={() => console.log('onPause callback')}
+        onEnded={() => console.log('onEnded callback')}
+        onError={() => console.log('onError callback')}
+       />
       
     </div>
   );
